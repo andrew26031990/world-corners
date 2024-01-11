87d7a0f8cf2fc77a5fda48b2845a57ddfc937f47
Andrew <mandrew3601@gmail.com>
2024-01-11 13:00:10 +0500

    changed welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,7 +62,12 @@
             </div>
             <div class="col-4 d-flex justify-content-end align-items-center">
                 <a class="link-secondary" href="#" aria-label="Search">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
+                         stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img"
+                         viewBox="0 0 24 24"><title>Search</title>
+                        <circle cx="10.5" cy="10.5" r="7.5"/>
+                        <path d="M21 21l-5.2-5.2"/>
+                    </svg>
                 </a>
                 <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
             </div>
@@ -81,23 +86,18 @@
 <main class="container">
     <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
         <div class="col-md-6 px-0">
-            <!-- SAPE RTB JS -->
-            <script
-                async="async"
-                src="https://cdn-rtb.sape.ru/rtb-b/js/868/2/137868.js"
-                type="text/javascript">
-            </script>
-            <!-- SAPE RTB END -->
-                {{--@php $article = getArticles()[0]; @endphp
-                @if(isset(getArticles()[0]))
-                    <h1 class="display-4 fst-italic">{{$article->title}}</h1>
-                    <p class="lead my-3">{{\Str::limit($article->short_text, 100)}}</p>
-                    <p class="lead mb-0"><a href="{{$article->slug}}" class="text-white fw-bold">Продолжить чтение...</a></p>
-                @else
-                    <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
-                    <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-                    <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
-                @endif--}}
+            @php $article = getArticles()[0] ?? null; @endphp
+            @if(isset($article))
+                <h1 class="display-4 fst-italic">{{$article->title}}</h1>
+                <p class="lead my-3">{{\Str::limit($article->short_text, 100)}}</p>
+                <p class="lead mb-0"><a href="{{$article->slug}}" class="text-white fw-bold">Продолжить чтение...</a>
+                </p>
+            @else
+                <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
+                <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and
+                    efficiently about what’s most interesting in this post’s contents.</p>
+                <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
+            @endif
         </div>
     </div>
 
@@ -105,42 +105,46 @@
         <div class="col-md-6">
             <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div class="col p-4 d-flex flex-column position-static">
-                    @php $articleTop1 = getArticles()[0]; @endphp
-                    @if(isset(getArticles()[0]))
+                    @php $articleTop1 = getArticles()[0] ?? null @endphp
+                    @if(isset($articleTop1))
                         <strong class="d-inline-block mb-2 text-primary">World</strong>
                         <h3 class="mb-0">{{$articleTop1->title}}</h3>
-                        <div class="mb-1 text-muted">{{\Carbon\Carbon::parse($articleTop1->created_at)->format('M d')}}</div>
+                        <div
+                            class="mb-1 text-muted">{{\Carbon\Carbon::parse($articleTop1->created_at)->format('M d')}}</div>
                         <p class="card-text mb-auto">{{\Str::limit($articleTop1->short_text, 91)}}</p>
                         <a href="{{$articleTop1->slug}}" class="stretched-link">Continue reading</a>
                     @else
                         <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
-                        <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
+                        <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly
+                            and efficiently about what’s most interesting in this post’s contents.</p>
                         <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
                     @endif
                 </div>
                 <div class="col-auto d-none d-lg-block">
-                    <img src="{{asset('thumbnail.png')}}" />
+                    <img src="{{asset('thumbnail.png')}}"/>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div class="col p-4 d-flex flex-column position-static">
-                    @php $articleTop2 = getArticles()[1]; @endphp
-                    @if(isset(getArticles()[1]))
+                    @php $articleTop2 = getArticles()[1] ?? null @endphp
+                    @if(isset($articleTop2))
                         <strong class="d-inline-block mb-2 text-primary">World</strong>
                         <h3 class="mb-0">{{$articleTop2->title}}</h3>
-                        <div class="mb-1 text-muted">{{\Carbon\Carbon::parse($articleTop2->created_at)->format('M d')}}</div>
+                        <div
+                            class="mb-1 text-muted">{{\Carbon\Carbon::parse($articleTop2->created_at)->format('M d')}}</div>
                         <p class="card-text mb-auto">{{\Str::limit($articleTop2->short_text, 91)}}</p>
                         <a href="{{$articleTop2->slug}}" class="stretched-link">Continue reading</a>
                     @else
                         <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
-                        <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
+                        <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly
+                            and efficiently about what’s most interesting in this post’s contents.</p>
                         <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
                     @endif
                 </div>
                 <div class="col-auto d-none d-lg-block">
-                    <img src="{{asset('thumbnail.png')}}" />
+                    <img src="{{asset('thumbnail.png')}}"/>
                 </div>
             </div>
         </div>
@@ -159,13 +163,11 @@
 </main>
 
 <footer class="blog-footer">
-    <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    <p>This blog is about world corners. <a href="/">{{app()->env('APP_NAME')}}</a>.
+    </p>
     <p>
         <a href="#">Back to top</a>
     </p>
 </footer>
-
-
-
 </body>
 </html>
