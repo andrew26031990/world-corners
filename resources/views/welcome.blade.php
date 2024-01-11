b649fc4f8b1ff3e6da95c6b9387e001188f93821
Andrew <mandrew3601@gmail.com>
2024-01-11 10:11:13 +0500

    changed fields.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,14 @@
 <main class="container">
     <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
         <div class="col-md-6 px-0">
-                @php $article = getArticles()[0]; @endphp
+            <!-- SAPE RTB JS -->
+            <script
+                async="async"
+                src="https://cdn-rtb.sape.ru/rtb-b/js/868/2/137868.js"
+                type="text/javascript">
+            </script>
+            <!-- SAPE RTB END -->
+                {{--@php $article = getArticles()[0]; @endphp
                 @if(isset(getArticles()[0]))
                     <h1 class="display-4 fst-italic">{{$article->title}}</h1>
                     <p class="lead my-3">{{\Str::limit($article->short_text, 100)}}</p>
@@ -90,7 +97,7 @@
                     <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
                     <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
                     <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
-                @endif
+                @endif--}}
         </div>
     </div>
 
