bf9bcb2eea7ed25e73601b1e6c778b393956518d
amagzumov <amagzumov@paymart.uz>
2024-02-27 14:26:37 +0500

    updates

---
+++ app/Console/Commands/UpdateLocationCreatedAt.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Location;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+
+class UpdateLocationCreatedAt extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:location-created-at';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update created_at for all locations with random dates';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $locations = Location::all(); // Get all locations
+
+        foreach ($locations as $location) {
+            $randomDate = Carbon::createFromDate(2022, 1, 1)->random(Carbon::now());
+            $location->created_at = $randomDate;
+            $location->save();
+
+            $this->info("Updated location (ID: {$location->id}) with date {$randomDate}");
+        }
+
+        $this->info('All locations updated successfully.');
+    }
+}
