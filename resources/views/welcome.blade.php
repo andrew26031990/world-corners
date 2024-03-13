147d2cd8def5af29adeb9312a207003d7fbb5b00
amagzumov <amagzumov@paymart.uz>
2024-03-13 11:53:18 +0500

    updated robots.txt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,6 +18,8 @@
     <meta name="author" content="Andrew Magzumov">
     <meta name="octoclick-verification" content="5398c64971658027e189c4dbb666a6f4">
 
+    <meta name="robots" content="index, follow">
+
     <meta property="og:title" content="{{$location->title ?? config('app.name')}}" />
     <meta property="og:type" content="article" />
     <meta property="og:image" content="{{asset('thumbnail.png')}}" />
