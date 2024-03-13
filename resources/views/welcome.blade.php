9e897e03725ddfd88107c1a2495dbcba59d8bdb9
amagzumov <amagzumov@paymart.uz>
2024-03-13 11:33:16 +0500

    updated robots.txt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,14 +32,6 @@
     <link href="{{asset('blog/bootstrap.min.css')}}" rel="stylesheet">
 
     <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
-        }
-
         a {
             color: #FFA500
         }
@@ -50,9 +42,7 @@
         }
 
         @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
+
         }
     </style>
 
