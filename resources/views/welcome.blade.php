9080a69238a2b5e22b781b7a968cdcd021a4d44a
amagzumov <amagzumov@paymart.uz>
2024-07-22 13:38:40 +0500

    updated welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
     <meta name="Published" content="{{$location->created_at ?? \Carbon\Carbon::now()}}">
     <meta name="Modified" content="{{$location->updated_at ?? \Carbon\Carbon::now()}}">
 
-    <title>{{$location->title ?? config('app.name')}}</title>
+    <title>{{$menu->name ? $location->title : config('app.name')}}</title>
 
     <link rel="icon" type="image/png" href="{{asset('blog/favicon.ico')}}" sizes="32x32">
     <link rel="canonical" href="{{config('app.url')}}" />
