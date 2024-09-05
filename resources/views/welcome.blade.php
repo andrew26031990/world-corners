6b206ffa01a87c70c51e9c966af2553226d3cc0b
amagzumov <amagzumov@paymart.uz>
2024-09-05 17:45:30 +0500

    updated fields.blade.php, welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -201,107 +201,43 @@
                 <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
             @endif
         </div>
-        <div class="row mb-2">
-            <div class="col-md-6">
-                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
-                        <h3 class="mb-0">Featured post</h3>
-                        <div class="mb-1 text-body-secondary">Nov 12</div>
-                        <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
-                            Continue reading
-                            <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
-                        </a>
-                    </div>
-                    <div class="col-auto d-none d-lg-block">
-                        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
-                        <h3 class="mb-0">Post title</h3>
-                        <div class="mb-1 text-body-secondary">Nov 11</div>
-                        <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
-                            Continue reading
-                            <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
-                        </a>
-                    </div>
-                    <div class="col-auto d-none d-lg-block">
-                        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
     <div class="row mb-2">
-        {{--<div class="col-md-6">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style="height: auto">
+        <div class="col-md-6">
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div class="col p-4 d-flex flex-column position-static">
-                    <!-- Yandex.RTB R-A-8796197-1 -->
-                    <div id="yandex_rtb_R-A-8796197-1"></div>
-                    <script>
-                        window.yaContextCb.push(()=>{
-                            Ya.Context.AdvManager.render({
-                                "blockId": "R-A-8796197-1",
-                                "renderTo": "yandex_rtb_R-A-8796197-1"
-                            })
-                        })
-                    </script>
+                    <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
+                    <h3 class="mb-0">Featured post</h3>
+                    <div class="mb-1 text-body-secondary">Nov 12</div>
+                    <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                        Continue reading
+                        <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+                    </a>
                 </div>
-                <div class="col p-4 d-flex flex-column position-static">
-                    <!-- Yandex.RTB R-A-8796197-1 -->
-                    <div id="yandex_rtb_R-A-8796197-4"></div>
-                    <script>
-                        window.yaContextCb.push(()=>{
-                            Ya.Context.AdvManager.render({
-                                "blockId": "R-A-8796197-4",
-                                "renderTo": "yandex_rtb_R-A-8796197-4"
-                            })
-                        })
-                    </script>
-                    <!-- SAPE RTB DIV ADAPTIVE -->
-                    --}}{{--<div id="SRTB_888041"></div>--}}{{--
-                    <!-- SAPE RTB END -->
+                <div class="col-auto d-none d-lg-block">
+                    <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style="height: auto">
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div class="col p-4 d-flex flex-column position-static">
-                    <!-- Yandex.RTB R-A-8796197-2 -->
-                    <div id="yandex_rtb_R-A-8796197-2"></div>
-                    <script>
-                        window.yaContextCb.push(()=>{
-                            Ya.Context.AdvManager.render({
-                                "blockId": "R-A-8796197-2",
-                                "renderTo": "yandex_rtb_R-A-8796197-2"
-                            })
-                        })
-                    </script>
+                    <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
+                    <h3 class="mb-0">Post title</h3>
+                    <div class="mb-1 text-body-secondary">Nov 11</div>
+                    <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                        Continue reading
+                        <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+                    </a>
                 </div>
-                <div class="col p-4 d-flex flex-column position-static">
-                    <!-- Yandex.RTB R-A-8796197-5 -->
-                    <div id="yandex_rtb_R-A-8796197-5"></div>
-                    <script>
-                        window.yaContextCb.push(()=>{
-                            Ya.Context.AdvManager.render({
-                                "blockId": "R-A-8796197-5",
-                                "renderTo": "yandex_rtb_R-A-8796197-5"
-                            })
-                        })
-                    </script>
-                    <!-- SAPE RTB DIV ADAPTIVE -->
-                    --}}{{--<div id="SRTB_888043"></div>--}}{{--
-                    <!-- SAPE RTB END -->
+                <div class="col-auto d-none d-lg-block">
+                    <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
                 </div>
             </div>
-        </div>--}}
+        </div>
     </div>
 
     <div class="row g-5">
