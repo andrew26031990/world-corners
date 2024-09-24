805369642b0cf206fd39472c38a6352d3eb376a2
amagzumov <amagzumov@paymart.uz>
2024-09-24 10:13:39 +0500

    updated site

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,8 @@
     <!-- main css -->
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    {{--<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>--}}
 </head>
 
 <body>
